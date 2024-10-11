d8411fc719d082ee8cb7817b3bd4142cc91d85c9
michael-shambel <Michaelshambel10@gmail.com>
2024-10-11 03:02:14 +0300

    footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { FaFacebook } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa6";
-import { FaLinkedin } from "react-icons/fa";
-import FooterImg from "../../assets/website/footer-bg.png"
+import FooterImg from "../../assets/website/footer-bg.png";
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaMapMarker } from "react-icons/fa";
+import { MdEmail, MdCall} from "react-icons/md";
 
 const bgImg = {
     backgroundImage: `url(${FooterImg})`,
@@ -16,18 +15,31 @@ const Footer = () => {
     return (
         <div style={bgImg} className="taxt-black mt-20 shadow-lg">
             <div className="bg-primary/20 dark:bg-gray-900/70 dark:text-white min-h-[400px]">
-                <div className="container grid md:grid-cols-3 pb-20 pt-5">
+                <div className="container grid md:grid-cols-2 pb-20 pt-5">
                     <div>
-                        <a href="#" className="font-semibold tracking-widest text-xl sm:text-3xl">
+                        <a href="#" className="uppercase border-b-8 border-primary font-semibold tracking-widest text-xl sm:text-3xl">
                             {" "}
-                            Kansas Treding
+                            Kansas Trading
                         </a>
-                        <p className="pt-4">
+                        <div className="pt-4 text-center flex text-primary">
                             {" "}
-                             Lemi Kura Sub-city,
+                             <FaMapMarker />Lemi Kura Sub-city, Summit-Goro road,
                              Infront of Saudi Arebia Embassy,
                              Addis Ababa Ethiopia
-                        </p>
+                        </div>
+                        <div className="flex items-center gap-4 text-primary">
+                            <MdCall /> +251987654321
+                        </div>
+                        <div className="flex items-center gap-4 text-primary">
+                            <MdEmail /> kansastreding@gmail.com
+                        </div>
+                        <a href="#" className="uppercase border-b-8 border-primary font-semibold tracking-widest text-xl sm:text-3xl"> {" "} Explore Our Social Media</a>
+                        <div className="pt-4 flex items-center text-primary gap-3 text-2xl">
+                            <FaFacebook/>
+                            <FaInstagram/>
+                            <FaLinkedin/>
+                            <FaTwitter/>
+                        </div>
                     </div>
                 </div>
             </div>
